e1297c160172d05ebb6c6be630504e5789d54851
Eran K <cactus921@gmail.com>
2020-04-16 13:24:04 +0300

    initial commit - up to vid. 26
    including 3 tests:
    1. Clear Color
    2. Texture 2D
    3. Cube 3D

---
+++ tests/Test.cpp
@@ -0,0 +1,39 @@
+#include "Test.h"
+
+
+
+#include <GL/glew.h>
+#include "imgui.h"
+
+
+
+namespace test{
+    TestMenu::TestMenu(Test*& currentTestPointer)
+        : m_CurrentTest (currentTestPointer)
+    {
+        //ctor
+
+    }
+
+    TestMenu::~TestMenu()
+    {
+        //dtor
+    }
+
+    void TestMenu::OnRender()
+    {
+        GLCALL(glClearColor(0.5f, 0.7f, 1.0f, 1.0f));
+        GLCALL(glClear(GL_COLOR_BUFFER_BIT));
+    }
+
+    void TestMenu::OnImGuiRender()
+    {
+        for(auto& test : m_Tests)
+        {
+            if(ImGui::Button(test.first.c_str()))
+                m_CurrentTest = test.second();
+        }
+    }
+
+}
+
